97e690c774034fa4efabc92150eac331acaec354
LeoNellus8915 <leonellus8915@gmail.com>
2022-04-21 13:39:24 +0200

    Bella

--- src/main/java/com/ProgettoScommesse/ScommesseRpctech/ScommesseRpctechApplication.java
+++ src/main/java/com/ProgettoScommesse/ScommesseRpctech/ScommesseRpctechApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScommesseRpctechApplication {
+public class ScommesseRpctechApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ScommesseRpctechApplication.class, args);
 	}
 
