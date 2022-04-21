b96dd769365fe63a1fbc4872703c13631baaa2e2
Alex07071984 <alessandro.ancinelli@gmail.com>
2022-04-21 13:31:47 +0200

    Progetto GitHub

---
+++ src/main/java/com/ProgettoScommesse/ScommesseRpctech/ScommesseRpctechApplication.java
@@ -0,0 +1,13 @@
+package com.ProgettoScommesse.ScommesseRpctech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScommesseRpctechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScommesseRpctechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProgettoScommesse/ScommesseRpctech/ScommesseRpctechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProgettoScommesse.ScommesseRpctech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScommesseRpctechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
