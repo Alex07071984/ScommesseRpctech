8f98a76e3661488a1b10aad522d34934aa44ef4c
M1k390 <michele.pollidoro@gamil.com>
2022-04-22 10:55:07 +0200

    Test Commit

--- src/main/java/com/ProgettoScommesse/controller/ScommesseRepository.java
+++ src/main/java/com/ProgettoScommesse/controller/ScommesseRepository.java
@@ -1,5 +1,9 @@
 package com.ProgettoScommesse.controller;
 
-public class ScommesseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Scommesse.ScommesseDomain.Utente;
+
+public class ScommesseRepository extends JpaRepository<Utente,Long>{
 
 }
