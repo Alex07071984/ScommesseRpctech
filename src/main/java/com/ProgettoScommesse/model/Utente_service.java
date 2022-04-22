d35bb56922f56a548d59ad588443ae5462929e25
AlessioDM7 <fieroadm5@gmail.com>
2022-04-22 12:12:17 +0200

    nuji

---
+++ src/main/java/com/ProgettoScommesse/model/Utente_service.java
@@ -0,0 +1,9 @@
+package com.ProgettoScommesse.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Utente_service extends JpaRepository <Utente,Integer> {
+	
+	
+
+}
